c3519f0d60b96e854f411b33d7841af3b43f5f0c
rashad <rland7596@gmail.com>
2020-08-18 10:43:50 +0000

    comment structure and attached image

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -43,19 +43,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/form-content")
 public class DataServlet extends HttpServlet {
 
-private static class Message {
+private static class Comment {
     private String uniqueKey;
-    private String message;
-    //private String imageUrl;
+    private String comment;
+    private String imageUrl;
     private long timestamp;
 
-    public Message(Key uniqueKey, String message, long time) {
+    public Comment(Key uniqueKey, String comment, String imageUrl, long time) {
       this.uniqueKey = KeyFactory.keyToString(uniqueKey);
-      this.message = message;
-      //this.imageUrl = imageUrl;
+      this.comment = comment;
+      this.imageUrl = imageUrl;
       this.timestamp = time;
     }
-  }
+}
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -65,22 +65,23 @@ private static class Message {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
 
-   ArrayList<Message> tasks = new ArrayList<>();
+   ArrayList<Comment> tasks = new ArrayList<>();
 
     for (Entity entity : results.asIterable()) {
-    Message newMessage = new Message((Key) entity.getKey(),
-                                        (String) entity.getProperty("text"),
-                                        (long) entity.getProperty("timestamp"));
+
+      Key uniqueKey = (Key) entity.getKey();
+      String comment = (String) entity.getProperty("comment");
+      long timestamp = (long) entity.getProperty("timestamp");
+      String picture = (String) entity.getProperty("imageUrl");
+
+    Comment newMessage = new Comment(uniqueKey, comment, picture, timestamp);
         
-     // String picture = (String) entity.getProperty("imageUrl");
-     
       tasks.add(newMessage);
-      //tasks.add(pic);
     }
     
     Gson gson = new Gson();
     // Respond with the result.
-    response.setContentType("text/html;");
+     response.setContentType("application/json;");
     response.getWriter().println(gson.toJson(tasks));
     }
 
@@ -88,55 +89,56 @@ private static class Message {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
     
-    String text = request.getParameter("text-input");
+    String comment = request.getParameter("message");
+    String imageUrl = getUploadedFileUrl(request, "image");
     long timestamp = System.currentTimeMillis();
 
-    //String imageUrl = getUploadedFileUrl(request, "image");
-
-    Entity message = new Entity("message");
-    message.setProperty("text", text);
+    Entity message = new Entity("Message");
+    message.setProperty("comment", comment);
     message.setProperty("timestamp", timestamp);
-   //message.setProperty("image", imageUrl);
+    message.setProperty("imageUrl", imageUrl);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(message);
+
+    response.sendRedirect("/index.html");
   }
 
-// ]  private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
-//     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-//     Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
-//     List<BlobKey> blobKeys = blobs.get("image");
+  private String getUploadedFileUrl(HttpServletRequest request, String formInputElementName) {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get("image");
 
-//     // User submitted form without selecting a file, so we can't get a URL. (dev server)
-//     if (blobKeys == null || blobKeys.isEmpty()) {
-//       return null;
-//     }
+    // User submitted form without selecting a file, so we can't get a URL. (dev server)
+    if (blobKeys == null || blobKeys.isEmpty()) {
+      return null;
+    }
 
-//     // Our form only contains a single file input, so get the first index.
-//     BlobKey blobKey = blobKeys.get(0);
+    // Our form only contains a single file input, so get the first index.
+    BlobKey blobKey = blobKeys.get(0);
 
-//     // User submitted form without selecting a file, so we can't get a URL. (live server)
-//     BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
-//     if (blobInfo.getSize() == 0) {
-//       blobstoreService.delete(blobKey);
-//       return null;
-//     }
+    // User submitted form without selecting a file, so we can't get a URL. (live server)
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return null;
+    }
 
-//     // We could check the validity of the file here, e.g. to make sure it's an image file
-//     // https://stackoverflow.com/q/10779564/873165
+    // We could check the validity of the file here, e.g. to make sure it's an image file
+    // https://stackoverflow.com/q/10779564/873165
 
-//     // Use ImagesService to get a URL that points to the uploaded file.
-//     ImagesService imagesService = ImagesServiceFactory.getImagesService();
-//     ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
-//     String url = imagesService.getServingUrl(options);
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+    String url = imagesService.getServingUrl(options);
 
-//     // GCS's localhost preview is not actually on localhost,
-//     // so make the URL relative to the current domain.
-//     if(url.startsWith("http://localhost:8080/")){
-//       url = url.replace("http://localhost:8080/", "/");
-//     }
-//     return url;
-//   }
+    // GCS's localhost preview is not actually on localhost,
+    // so make the URL relative to the current domain.
+    if(url.startsWith("http://localhost:8080/")){
+      url = url.replace("http://localhost:8080/", "/");
+    }
+    return url;
+  }
 
   private String getParameter(HttpServletRequest request, String name, String defaultValue) {
     String value = request.getParameter(name);
